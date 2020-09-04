3c4cb9fd63f9159d9a121f8c318542a36d3ccfcd
fred1653 <fred1653@qq.com>
2020-09-04 15:08:53 +0800

    add read and write

--- 3rd/tinyServer/tiny.cpp
+++ 3rd/tinyServer/tiny.cpp
@@ -10,12 +10,13 @@
 #include <unistd.h>
 
 #include <csignal>
+#include <cstring>
 #include <system_error>
 #include <ctime>
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-#inlcude <iostream>
+#include <iostream>
 #include <map>
 
 #define LISTENQ 1024  /* second argument to listen() */
@@ -23,8 +24,8 @@
 #define RIO_BUFSIZE 1024
 
 typedef struct {
-    size_t rio_fd;             /* descriptor for this buf */
-    size_t rio_cnt;            /* next unread byte in this buf */
+    int rio_fd;             /* descriptor for this buf */
+    int rio_cnt;            /* next unread byte in this buf */
     char *rio_bufptr;          /* next unread byte in this buf */
     char rio_buf[RIO_BUFSIZE]; /* internal buffer */
 } rio_t;
@@ -56,9 +57,30 @@ std::map<std::string, std::string> mime_map {
 
 std::string const default_mime_type("text/plain");
 
-void rio_readinitb(rio_t *rp, int fd) {}
+void rio_readinitb(rio_t *rp, int fd) {
+    rp->rio_fd = fd;
+    rp->rio_cnt = 0;
+    rp->rio_bufptr = rp->rio_buf;
+}
 
-ssize_t writen(int fd, void *usrbuf, size_t n) {}
+ssize_t writen(int fd, void *usrbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = static_cast<char *>(usrbuf);
+
+    while(nleft > 0) {
+        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR) {
+                nwritten = 0;
+            } else {
+                return -1;
+            }
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
+    }
+    return n;
+}
 
 /*
  * rio_read - This is a wrapper for the Unix read() function that
@@ -69,13 +91,57 @@ ssize_t writen(int fd, void *usrbuf, size_t n) {}
  *    read() if the internal buffer is empty.
  */
 /* $begin rio_read */
-static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n) {}
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n) {
+    int cnt;
+    while(rp->rio_cnt <= 0) {
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, sizeof(rp->rio_buf));
+
+        if (rp->rio_cnt < 0) {
+            if (errno != EINTR) {
+                return -1;
+            }
+        } else if (rp->rio_cnt == 0) {
+            return 0;
+        } else {
+            rp->rio_bufptr = rp->rio_buf;
+        }
+    }
+    /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
+    cnt = n;
+    if (rp->rio_cnt < n) {
+        cnt = rp->rio_cnt;
+    }
+    memcpy(usrbuf, rp->rio_bufptr, cnt);
+    rp->rio_bufptr += cnt;
+    rp->rio_cnt -= cnt;
+    return cnt;
+}
 
 
 /*
  * rio_readlineb - robustly read a text line (buffered)
  */
-ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {}
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {
+    int n, rc;
+    char c, *bufp = static_cast<char *>(usrbuf);
+
+    for (n = 1; n < maxlen; n++) {
+        if ((rc = rio_read(rp, &c, 1)) == 1) {
+            *bufp++ = c;
+            if (c == '\n')
+                break;
+        } else if (rc == 0) {
+            if (n == 1)
+                return 0;
+            else
+                break;
+        } else {
+            return -1;
+        } 
+    }
+    *bufp = 0;
+    return n;
+}
 
 void format_size(char *buf, struct stat *stat) {
     if (S_ISDIR(stat->st_mode)) {
